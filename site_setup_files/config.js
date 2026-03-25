586a4ff50d8aacac152aba21d71c5a7cf063aef5
TheDustySigmaLens <linda.atty@yahoo.com>
2026-03-25 11:18:08 -0400

    Update config.js

--- site_setup_files/config.js
+++ site_setup_files/config.js
@@ -2,3 +2,7 @@ window.APP_CONFIG = {
   SUPABASE_URL: "",
   SUPABASE_ANON_KEY: ""
 };
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://wbhccslxptnwknliwech.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_mHs2hc8j1hvYV871Sf0aSw_4FD6icY4"
+};
